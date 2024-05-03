0bd7bcb832263a7c95c7e71e6a89379bcbcbd5e7
Mahdi-Marv <mahdihajialilue@gmail.com>
2024-05-03 12:14:22 +0330

    fix bug

--- models/transform_layers.py
+++ models/transform_layers.py
@@ -97,9 +97,9 @@ class RandomResizedCropLayer(nn.Module):
 
         grid = F.affine_grid(_theta, inputs.size(), **kwargs).to(_device)
         output = F.grid_sample(inputs, grid, padding_mode='reflection', **kwargs)
-        print(output.size)
-        if self.size is not None:
-            output = F.adaptive_avg_pool2d(output, self.size)
+        print(self.size)
+        # if self.size is not None:
+        #     output = F.adaptive_avg_pool2d(output, self.size)
 
         return output
 
